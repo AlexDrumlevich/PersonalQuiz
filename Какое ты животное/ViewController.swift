c2d22021992d67c7f732d33b8b75cfcc1ea75c79
Друмлевич <drumlevic@macbook-pro-drumlevic.local>
2018-12-05 20:43:50 +0500

    Initial Commit

---
+++ Какое ты животное/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Какое ты животное
+//
+//  Created by Друмлевич on 05.12.2018.
+//  Copyright © 2018 Alexey Drumlevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
